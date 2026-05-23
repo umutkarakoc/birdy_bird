a8749a235fb8b9106c755f4016ad2c0036bb6380
umut <umut@sent.com>
2026-05-23 12:39:24 +0300

    bird anim

--- src/main.zig
+++ src/main.zig
@@ -28,12 +28,17 @@ const Game = struct {
     dt: f32 = 0,
 
     // Bird
+    bird_w: f32 = 216,
+    bird_h: f32 = 150,
     gravity: f32 = 10.0,
     jump_force: f32 = 100.0,
     move_speed: f32 = 100.0,
+    bird_anim_index: u16 = 0,
+    bird_anim_time: f32 = 0,
+    bird_pos: rl.Vector2 = .{ .x = 100, .y = 360 },
 
     // Background
-    bg_size: rl.Vector2 = .{ .x = 400, .y = 715 },
+    bg_size: rl.Vector2 = .{ .x = 400, .y = 720 },
     bg_scroll_speed: f32 = -50.0,
     bg_scroll: f32 = 0.0,
 
@@ -50,6 +55,7 @@ const Game = struct {
 
     pub fn update(game: *Game) void {
         game.update_bg();
+        game.update_bird();
         if (!game.is_started) {
             return;
         }
@@ -62,12 +68,21 @@ const Game = struct {
         }
     }
 
+    pub fn update_bird(game: *Game) void {
+        game.bird_anim_time += game.dt;
+        if (game.bird_anim_time >= 1.0) {
+            game.bird_anim_time = 0.0;
+        }
+        game.bird_anim_index = @intFromFloat(game.bird_anim_time * 20.0);
+    }
+
     pub fn draw(game: Game) void {
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(.ray_white);
         game.draw_bg();
+        game.draw_bird();
         game.draw_obstacle();
     }
 
@@ -84,5 +99,13 @@ const Game = struct {
 
     pub fn draw_obstacle(game: Game) void {
         _ = game;
+    }
+
+    pub fn draw_bird(game: Game) void {
+        rl.drawTextureV(
+            game.sprites.bird[game.bird_anim_index],
+            .{ .x = game.bird_pos.x, .y = game.bird_pos.y - (game.bird_h / 2) },
+            .ray_white,
+        );
     }
 };
