c2aa15649d8293169123dac86955555ac9ef09d9
umut <umut@sent.com>
2026-05-23 10:54:11 +0300

    asset loader

---
+++ src/assets.zig
@@ -0,0 +1,55 @@
+const std = @import("std");
+const rl = @import("raylib");
+
+pub const Sprites = struct {
+    bird: [20]rl.Texture2D,
+    dead: [20]rl.Texture2D,
+    obstacle: rl.Texture2D,
+    bg: rl.Texture2D,
+    play: rl.Texture2D,
+
+    fn load() !Sprites {
+        var bird: [20]rl.Texture2D = undefined;
+
+        for (&bird, 0..) |*tex, i| {
+            var buf: [64]u8 = undefined;
+            const path = std.fmt.bufPrintZ(
+                &buf,
+                "assets/bird/{d:0>2}.png",
+                .{i},
+            ) catch unreachable;
+            tex.* = rl.loadTexture(path);
+        }
+
+        var dead: [20]rl.Texture2D = undefined;
+        for (&dead, 0..) |*tex, i| {
+            var buf: [64]u8 = undefined;
+            const path = std.fmt.bufPrintZ(
+                &buf,
+                "assets/dead/{d:0>2}.png",
+                .{i},
+            ) catch unreachable;
+            tex.* = rl.loadTexture(path);
+        }
+
+        return .{
+            .bird = bird,
+            .dead = dead,
+            .bg = try rl.loadTexture("assets/bg.png"),
+            .play = try rl.loadTexture("assets/play .png"),
+            .obstacle = try rl.loadTexture("assets/obstacle .png"),
+        };
+    }
+
+    pub fn unload(self: Sprites) void {
+        for (self.bird) |tex| {
+            rl.unloadTexture(tex);
+        }
+        for (self.dead) |tex| {
+            rl.unloadTexture(tex);
+        }
+        rl.unloadTexture(self.bg);
+        rl.unloadTexture(self.play);
+        rl.unloadTexture(self.obstacle);
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,63 @@
 const rl = @import("raylib");
+const assets = @import("assets.zig");
 
-const sw = 1920;
-const sh = 1080;
+const Game = struct {
+    // Screen
+    sw: i32 = 1920,
+    sh: i32 = 1080,
+    dt: f32 = 0,
 
-pub fn main() void {
-    rl.initWindow(sw, sh, "Birdy bird");
+    // Bird
+    gravity: f32 = 10.0,
+    jump_force: f32 = 100.0,
+    move_speed: f32 = 100.0,
+
+    // Background
+    bg_size: rl.Vector2 = .{ .x = 400, .y = 715 },
+    bg_scroll: f32 = 20.0,
+
+    // Obstacle
+    obs_size: rl.Vector2 = .{ .x = 105, .y = 338 },
+    obs_gap_y: f32 = 60,
+
+    // Game State
+    is_started: bool = false,
+    score: i32 = 0,
+
+    //assets
+    sprites: assets.Sprites,
+
+    pub fn update(game: Game) void {
+        if (!game.is_started) {
+            return;
+        }
+    }
+
+    pub fn draw() void {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(.ray_white);
+        draw_obstacle();
+    }
+    pub fn draw_obstacle() void {}
+};
+
+pub fn main() !void {
+    rl.initWindow(1920, 1080, "Birdy bird");
     defer rl.closeWindow();
 
+    var sprites: assets.Sprites = assets.Sprites.load();
+    defer sprites.unload();
+
+    var game: Game = .{.assets};
+
+    var t = try rl.loadTexture("assets/obstacle.png");
+    defer t.unload();
+
     while (!rl.windowShouldClose()) {
-        update();
-        draw();
+        game.dt = rl.getFrameTime();
+        game.update();
+        game.draw();
     }
-}
-
-pub fn update() void {
-    
-}
-
-pub fn draw() void {
-    rl.beginDrawing();
-    defer rl.endDrawing();
-
-    rl.clearBackground(.ray_white);
 }
